3e0f63331488f5943d0ba4165e16fc091a9500b7
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-27 13:59:59 +0200

    Set basic layout
    Create basic layout with classes and id's.

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(op, a, b) {
+    switch (op) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+}
