6f5f90eee2d4ab04dd83f2bedd17146fb31a93d1
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-28 19:00:53 +0200

    Manage input number
    Create logic for function that receive number as input and update main display.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-let firstNumber = 0;
-let secondNumber = 0;
+let numberA = 0;
+let numberB = 0;
+let result = 0;
 let operator = "+";
 
 const bigDisplay = document.querySelector(".main");
@@ -9,34 +10,73 @@ const smallDisplay = document.querySelector(".support");
 function operate() {
     switch (operator) {
         case '+':
-            return secondNumber + firstNumber;
+            result = numberA + numberB;
             break;
         case '-':
-            return secondNumber - firstNumber;
+            result = numberA - numberB;
             break;
         case '×':
-            return secondNumber * firstNumber;
+            result = numberA * numberB;
             break;
         case '÷':
-            return secondNumber / firstNumber;
+            result = numberA / numberB;
             break;
     }
 }
 
 function updateOperator(operator) {
-    updateFirstNumber()
+    updateNumberA()
     smallDisplay.textContent = `${firstNumber} ${operator}`;
     // to do
-    console.log("to do");
+    console.log("updateOperator to do");
 }
 
-function updateFirstNumber() {
+function updateNumberA() {
     // to do
-    console.log("to do");
+    console.log("updateNumberA to do");
+}
+
+function updateNumberB() {
+    // to do
+    console.log("updateNumberB to do");
 }
 
 function getDisplay() {
     return bigDisplay.textContent;
+}
+
+function updateMainDisplay(symbol) {
+    let input = getDisplay();
+    if (symbol === ".") {
+        if (input === "0")
+            bigDisplay.textContent = "0.";
+        else if (!input.includes("."))
+            bigDisplay.textContent = input + ".";
+    } else if (input === "0")
+        bigDisplay.textContent = symbol;
+    else
+        if (input.length < 11)
+            bigDisplay.textContent = input + symbol;
+    updateNumberA();
+    console.log("updateMainDisplay: " + getDisplay());
+}
+
+function updateSupportDisplay(text) {
+    // to do
+    console.log("updateSupportDisplay to do");
+}
+
+function backspace() {
+    // to do
+    console.log("backspace to do");
+}
+
+function clearDisplays() {
+    smallDisplay.innerHTML = "\&nbsp;";
+    bigDisplay.textContent = 0;
+    numberA = 0;
+    numberB = 0;
+    result = 0;
 }
 
 const buttons = document.querySelectorAll("button");
@@ -44,15 +84,21 @@ buttons.forEach(button => {
     button.addEventListener("click", function (e) {
         if (e.target.className === 'number') {
             console.log(e.target.textContent + " number");
-            updateFirstNumber(e.target.textContent);
+            updateMainDisplay(e.target.textContent);
         }
         else if (e.target.className === 'operator') {
             console.log(e.target.textContent + " operator");
             updateOperator(e.target.textContent);
         }
-        else
+        else {
             console.log(e.target.textContent + " action");
-
+            if (e.target.textContent === "AC") {
+                clearDisplays();
+            }
+            if (e.target.textContent === "backspace") {
+                backspace();
+            }
+        }
     })
 });
 
